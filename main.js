da54cc2b5a7f7c8f3a0007f8f6a8ff21e9b22c35
Sarah Currey <curreysarah28@gmail.com>
2018-08-20 21:49:08 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log ("User flipped" + " " +cardOne);
-console.log ("User flipped" + " " +cardTwo);
+var cards = ["queen", 'queen', 'king', 'king'];
+var cardsInPlay =[]
+
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+alert("You found a match!");
+} else {
+alert("Sorry, try again.");
+}
+};
+
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	cardsInPlay;
+
+if (cardsInPlay.length ===2) {
+	checkForMatch();
+}
+};
+
+flipCard(0);
+flipCard(2);
