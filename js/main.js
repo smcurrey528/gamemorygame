25379e9e3aa09f7ee7f938bbc6b6e5d23fa17c09
Sarah Currey <curreysarah28@gmail.com>
2018-08-20 21:25:31 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 var cards = ["queen", 'queen', 'king', 'king'];
 var cardsInPlay =[]
 
-cards[0]= "cardOne";
-cards;
-
-cardsInPlay.push("cardOne");
-cardsInPlay;
-
-console.log("User flipped queen");
-
-var cardTwo = "king";
-
-cards[2]="cardTwo";
-
-cardsInPlay.push("cardTwo");
-cardsInPlay;
-
-console.log("User flipped king");
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay [1]) {
-		alert("You've found a match!");
-	} else {
-		alert ("Sorry, try again");
-	}
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+alert("You found a match!");
+} else {
+alert("Sorry, try again.");
 }
+};
+
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	cardsInPlay;
+
+if (cardsInPlay.length ===2) {
+	checkForMatch();
+}
+};
+
+flipCard(0);
+flipCard(2);
+
+
+
+
 
